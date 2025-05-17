41e30939aa3df0a20607ff2e9a4d80eac4f05beb
Aziz Kurniawan <aziz.122140097@student.itera.ac.id>
2025-05-18 03:58:15 +0700

    fix

--- app/src/main/java/com/example/triviaquest/ui/theme/Color.kt
+++ app/src/main/java/com/example/triviaquest/ui/theme/Color.kt
@@ -2,12 +2,12 @@ package com.example.triviaquest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFFB0D0FF)
+val PurpleGrey80 = Color(0xFFBFC9D9)
+val Pink80 = Color(0xFFC8DFF1)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF4050A4)
+val PurpleGrey40 = Color(0xFF4F5B71)
+val Pink40 = Color(0xFF526D85)
 
-val PrimaryPurple = Color(0xFF6A5AE0)
+val PrimaryPurple = Color(0xFF5A8DE0)
